abfefb22c5a26bfc4f6d7412c35ce4fd1a192cde
kitahara <kitahara@edfa4d69-1941-0410-9382-b3d123132320>
2007-12-17 07:47:35 +0000

    making cmx.filewrappers.amusaj package
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@8 edfa4d69-1941-0410-9382-b3d123132320

---
+++ src/jp/crestmuse/cmx/filewrappers/amusaj/ByteArrayNodeInterface.java
@@ -0,0 +1,82 @@
+package jp.crestmuse.cmx.filewrappers.amusaj;
+import jp.crestmuse.cmx.filewrappers.*;
+import java.util.*;
+import java.nio.*;
+import org.w3c.dom.*;
+import org.apache.commons.codec.binary.*;
+
+public abstract class ByteArrayNodeInterface extends NodeInterface {
+  private int bytesize;
+  private ByteBuffer buff;
+  private static final Base64 base64 = new Base64();
+
+  protected ByteArrayNodeInterface(Node node) {
+    super(node);
+    bytesize = getAttributeInt("bytesize");
+    buff = ByteBuffer.wrap(base64.decodeBase64(getText().getBytes()));
+  }
+
+  protected final int lengthInByte() {
+    return bytesize;
+  }
+
+  protected final int lengthInShort() {
+    return bytesize / 2;
+  }
+
+  protected final int lengthInInt() {
+    return bytesize / 4;
+  }
+
+  protected byte[] getByteArray() {
+    byte[] array = new byte[lengthInByte()];
+    buff.get(array);
+//    for (int i = 0; i < array.length; i++)
+//      array[i] = buff.get();
+    return array;
+  }
+
+  protected short[] getUnsignedByteArray() {
+    short[] array = new short[lengthInByte()];
+    for (int i = 0; i < array.length; i++) {
+      byte b = buff.get();
+      array[i] = b < 0 ? (short)(b + 128) : b;
+    }
+    return array;
+  }
+
+  protected short[] getShortArray() {
+    short[] array = new short[lengthInShort()];
+    for (int i = 0; i < array.length; i++)
+      array[i] = buff.getShort();
+    return array;
+  }
+
+  protected int[] getUnsignedShortArray() {
+    int[] array = new int[lengthInShort()];
+    for (int i = 0; i < array.length; i++) {
+      short s = buff.getShort();
+      array[i] = s < 0 ? s + 32768 : s;
+    }
+    return array;
+  }
+
+  protected int[] getIntArray() {
+    int[] array = new int[lengthInInt()];
+    for (int i = 0; i < array.length; i++)
+      array[i] = buff.getInt();
+    return array;
+  }
+
+  protected void setByteOrder(ByteOrder bo) {
+    buff.order(bo);
+  }
+
+  public static void addByteArrayToWrapper(byte[] data, String nodename, 
+                                           CMXFileWrapper wrapper) {
+    String s = new String(base64.encodeBase64Chunked(data));
+    wrapper.addChild(nodename);
+    wrapper.setAttribute("bytesize", data.length);
+    wrapper.addText(s);
+  }
+}
---
+++ src/jp/crestmuse/cmx/filewrappers/amusaj/GenericHeader.java
@@ -0,0 +1,60 @@
+package jp.crestmuse.cmx.filewrappers.amusaj;
+import jp.crestmuse.cmx.filewrappers.*;
+import org.w3c.dom.*;
+import java.util.*;
+
+public class GenericHeader extends NodeInterface {
+  NodeList nodelist;
+  int size;
+  Map<String,String> map;
+  public GenericHeader(Node node) {
+    super(node);
+    nodelist = getChildNodes();
+    size = nodelist.getLength();
+    map = new HashMap<String,String>();
+  }
+
+  protected String getSupportedNodeName() {
+    return "header";
+  }
+
+  public String getHeaderElement(String name) {
+    if (map.containsKey(name)) {
+      return map.get(name);
+    } else {
+      for (int i = 0; i < size; i++) {
+        if (getAttribute(nodelist.item(i), "name").equals(name)) {
+          String content = getAttribute(nodelist.item(i), "content");
+          map.put(name, content);
+          return content;
+        }
+      }
+      return null;
+    }
+  }
+
+  public int getHeaderElementInt(String name) {
+    String content = getHeaderElement(name);
+    if (content == null)
+      return -1;
+    else
+      return Integer.parseInt(content);
+  }
+
+  public double getHeaderElementDouble(String name) {
+    String content = getHeaderElement(name);
+    if (content == null)
+      return Double.NaN;
+    else
+      return Double.parseDouble(content);
+  }
+
+  public static void addHeaderElementToWrapper(String name, String content, 
+                                               CMXFileWrapper wrapper) {
+    wrapper.addChild("meta");
+    wrapper.setAttribute("name", name);
+    wrapper.setAttribute("content", content);
+    wrapper.returnToParent();
+  }
+
+}
