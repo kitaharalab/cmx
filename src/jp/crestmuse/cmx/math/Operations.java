f95bdc3c882370a0ac92fa4fecc9b12f0ae7c4df
kitahara <kitahara@edfa4d69-1941-0410-9382-b3d123132320>
2007-12-17 14:02:17 +0000

    implementing signal processing classes
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@11 edfa4d69-1941-0410-9382-b3d123132320

---
+++ src/jp/crestmuse/cmx/math/Operations.java
@@ -0,0 +1,19 @@
+package jp.crestmuse.cmx.math;
+
+public class Operations {
+
+  private static final DoubleArrayFactory factory = 
+    DoubleArrayFactory.getFactory();
+
+  private Operations() { }
+
+
+  public static DoubleArray subarray(DoubleArray x, int from, int thru) {
+    int length = thru - from;
+    DoubleArray z = factory.createArray(length);
+    for (int i = 0; i < length; i++)
+      z.set(i, x.get(from + i));
+    return z;
+  }
+}
+            
