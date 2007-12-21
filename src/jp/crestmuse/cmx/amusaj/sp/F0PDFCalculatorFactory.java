f75b4a7787aa839f21342d52be99ada196a6392e
kitahara <kitahara@edfa4d69-1941-0410-9382-b3d123132320>
2007-12-21 13:55:00 +0000

    completed refactoring
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@28 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/amusaj/sp/F0PDFCalculatorFactory.java
+++ src/jp/crestmuse/cmx/amusaj/sp/F0PDFCalculatorFactory.java
@@ -3,7 +3,7 @@ package jp.crestmuse.cmx.amusaj.sp;
 public abstract class F0PDFCalculatorFactory extends SPFactory {
   public static F0PDFCalculatorFactory getFactory() {
     return (F0PDFCalculatorFactory)
-      getFactory("f0calcFactory", "F0PredominanceCalculatorFactoryImpl");
+      getFactory("f0pdfFactory", "F0PDFCalculatorFactoryImpl");
   }
 
   public abstract F0PDFCalculator 
