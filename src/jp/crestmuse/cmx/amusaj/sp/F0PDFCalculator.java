7d7eff5b5462ff1ce4798954b1df1b30918532de
kitahara <kitahara@edfa4d69-1941-0410-9382-b3d123132320>
2007-12-21 13:48:19 +0000

    refactored F0Predominance... to F0PDF...
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@26 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/amusaj/sp/F0PDFCalculator.java
+++ src/jp/crestmuse/cmx/amusaj/sp/F0PDFCalculator.java
@@ -2,6 +2,6 @@ package jp.crestmuse.cmx.amusaj.sp;
 import jp.crestmuse.cmx.amusaj.filewrappers.PeakSet;
 import jp.crestmuse.cmx.math.*;
 
-public interface F0PredominanceCalculator {
+public interface F0PDFCalculator {
   DoubleArray calcWeights(PeakSet peakset);
 }
