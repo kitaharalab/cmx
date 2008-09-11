8d1a158c5f36d003cf99f1c57123c9242d7da3a5
unagi <unagi@edfa4d69-1941-0410-9382-b3d123132320>
2008-09-11 16:48:45 +0000

    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@157 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/commands/test/Dev2NoteFormatTest.java
+++ src/jp/crestmuse/cmx/commands/test/Dev2NoteFormatTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import jp.crestmuse.cmx.filewrappers.CMXFileWrapper;
 import jp.crestmuse.cmx.filewrappers.DeviationInstanceWrapper;
 import jp.crestmuse.cmx.filewrappers.MusicXMLWrapper;
+import jp.crestmuse.cmx.misc.TreeView;
 
 public class Dev2NoteFormatTest {
 
@@ -19,7 +20,7 @@ public class Dev2NoteFormatTest {
 			//File file = new File("output.txt");
 			//BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 			
-			
+			/*
 			//DeviationXMLに対応するMusicXML取得
 			MusicXMLWrapper mus = dev.getTargetMusicXML();
 			
@@ -39,15 +40,37 @@ public class Dev2NoteFormatTest {
 							
 							DeviationInstanceWrapper.NoteDeviation currentdev = dev.getNoteDeviation((MusicXMLWrapper.Note)md);
 							// !! アタック取れたrelease, dynamics, end-dynamicsはこの要領でいける !!
-							if(currentdev != null) System.out.println(currentdev.attack());
+							//if(currentdev != null) System.out.println(currentdev.attack());
+							
+							
 							
 						}
 					}
 				}
 			}
+			*/
+			
+			TreeView<DeviationInstanceWrapper.Control> npartwise = dev.getNonPartwiseControlView();
+			npartwise.getRoot();
+			
+			while (npartwise.hasNextL()) {
+				while (npartwise.hasNextR()) {
+					System.out.println(npartwise.nextR().measure());
+					
+					//check
+					System.out.println(npartwise.hasElementsAtNextTime());
+					System.out.println(npartwise.hasNextL());
+					System.out.println(npartwise.hasNextR());
+				}
+				npartwise.nextL();
+			}
+			//check
+			System.out.println(npartwise.hasElementsAtNextTime());
+			System.out.println(npartwise.hasNextL());
+			System.out.println(npartwise.hasNextR());
 			
 			
-			//System.out.println(mus.getPartwiseNoteView().get(0).toString());
+			
 		}
 		catch(Exception e){
 			e.printStackTrace();
