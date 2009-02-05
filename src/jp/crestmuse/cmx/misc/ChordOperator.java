0b98f370fa493905284f3bb6f2c6eb03e8ea52eb
unagi <unagi@edfa4d69-1941-0410-9382-b3d123132320>
2009-02-05 03:54:04 +0000

    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@218 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/misc/ChordOperator.java
+++ src/jp/crestmuse/cmx/misc/ChordOperator.java
@@ -5,6 +5,12 @@ import java.util.Map;
 
 public class ChordOperator {
 
+  
+  
+  
+  
+  
+
   /**
    * 自動伴奏のコードSMFで用いる、ダイアトニックコードの根音から
    * 相対的なノートナンバーの差をint配列で返すメソッドです。
@@ -12,15 +18,15 @@ public class ChordOperator {
    * @return
    */
   static int[] diatonic_map_inC(String chord){
-    Map<String, int[]> diatonic_map = new HashMap<String, int[]>();
-    diatonic_map.put("C", new int[] {0, 0, 0});
-    diatonic_map.put("Dm", new int[] {2, 1, 2});
-    diatonic_map.put("Em", new int[] {4, 3, 4});
-    diatonic_map.put("F", new int[] {5, 5, 5});
-    diatonic_map.put("G", new int[] {7, 7, 7});
-    diatonic_map.put("Am", new int[] {9, 8, 9});
-    diatonic_map.put("Bdim", new int[] {11, 10, 10});
-
-    return diatonic_map.get(chord);
+    int[] diatonic = new int[3];
+    if(chord.equals("C")) diatonic = new int[] {0, 0, 0};
+    else if(chord.equals("Dm")) diatonic = new int[] {2, 1, 2};
+    else if(chord.equals("Em")) diatonic = new int[] {4, 3, 4};
+    else if(chord.equals("F")) diatonic = new int[] {5, 5, 5};
+    else if(chord.equals("G")) diatonic = new int[] {7, 7, 7};
+    else if(chord.equals("Am")) diatonic = new int[] {9, 8, 9};
+    else if(chord.equals("Bdim")) diatonic =  new int[] {11, 10, 10};
+    
+    return diatonic;
   }
 }
