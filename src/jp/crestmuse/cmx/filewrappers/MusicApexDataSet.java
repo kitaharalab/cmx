c280fe1cf3e07b0d759ed8e9a87388023e457ec9
unagi <unagi@edfa4d69-1941-0410-9382-b3d123132320>
2009-03-03 09:42:53 +0000

    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@326 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/filewrappers/MusicApexDataSet.java
+++ src/jp/crestmuse/cmx/filewrappers/MusicApexDataSet.java
@@ -1,30 +1,44 @@
 package jp.crestmuse.cmx.filewrappers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
-import javax.xml.transform.TransformerException;
-
 import jp.crestmuse.cmx.filewrappers.MusicXMLWrapper.Note;
-import jp.crestmuse.cmx.misc.TreeView;
 
 public class MusicApexDataSet {
 
   private MusicXMLWrapper musicxml = null;
-  private Boolean inherited = null;
+  private Boolean inherited = false;
   private ApexDataGroup grouptop = null;
+  private List<ApexDataGroup> groupcollection;
   
   public MusicApexDataSet(MusicXMLWrapper musicxml){
     this.musicxml = musicxml;
+    this.groupcollection = new ArrayList<ApexDataGroup>();
   }
   
   public NoteGroup createTopLevelGroup(Boolean inherited){
     this.inherited = inherited;
     this.grouptop = new ApexDataGroup();
     grouptop.depth = 1;
-    //TODO add all notes from MusicXML
-    
-   
-    //grouptop.addNote(n);
+
+    //MusicXMLのすべてのNote要素をグループに追加する
+    MusicXMLWrapper.Part[] partlist = musicxml.getPartList();
+    for (MusicXMLWrapper.Part part : partlist) {
+      MusicXMLWrapper.Measure[] measurelist = part.getMeasureList();
+      for (MusicXMLWrapper.Measure measure : measurelist) {
+        MusicXMLWrapper.MusicData[] mdlist = measure.getMusicDataList();
+        for (MusicXMLWrapper.MusicData md : mdlist) {
+          if(md instanceof MusicXMLWrapper.Note){
+            MusicXMLWrapper.Note note = (MusicXMLWrapper.Note)md; //Noteにダウンキャスト
+            grouptop.addNote(note);  
+          }
+        }
+      }
+    }
+    groupcollection.add(grouptop);
     return grouptop;
   }
   
@@ -37,18 +51,29 @@ public class MusicApexDataSet {
     //TODO wrap
     return maw;
   }
-  
+ 
+  public int getDepth(Note n){
+    int thisdepth = -1;
+    
+    
+    return thisdepth;
+  }
   
   class ApexDataGroup implements NoteGroup{
 
     private int depth;
-    private List<Note> notes;
-    private List<ApexDataGroup> subGroups;
-    private NoteGroup groupParent;
+    private List<Note> ownnotes;  //自分のグループが持つノート
+    private List<Note> undernotes;  //自分のグループ以下にあるノート、自分も含む
+    private List<NoteGroup> subGroups;
+    private NoteGroup groupParent = null;
     private Note apex = null;
     private Double saliency = null;
     
     public ApexDataGroup(){
+      depth = -1;
+      ownnotes = new ArrayList<Note>();
+      undernotes = new ArrayList<Note>();
+      subGroups = new ArrayList<NoteGroup>();
       return;
     }
 
@@ -69,13 +94,35 @@ public class MusicApexDataSet {
     
     @Override
     public List<Note> getNotes() {
-      return notes;
+      return ownnotes;
     }
     
     @Override
     public List<Note> getAllNotes() {
-      // TODO 自動生成されたメソッド・スタブ
-      return null;
+      return undernotes;
+      /*
+      //再帰できてるかなー
+      List<Note> notelist = new ArrayList<Note>();
+      if(getSubgroups().isEmpty()){
+        return getNotes();
+      }
+      else{
+        Iterator<NoteGroup> it = getSubgroups().iterator();
+        while(it.hasNext()){
+          NoteGroup g = it.next();
+          notelist.addAll(g.getAllNotes());
+        }
+      }
+      notelist.addAll(getNotes());
+      
+      //HashSetに一旦格納し重複除去
+      HashSet<Note> set = new HashSet<Note>();
+      set.addAll(notelist);
+      notelist = new ArrayList<Note>();
+      notelist.addAll(set);
+      
+      return notelist;
+      */
     }
 
     @Override
@@ -85,25 +132,33 @@ public class MusicApexDataSet {
 
     @Override
     public List<NoteGroup> getSubgroups() {
-      //TODO
-      return null;
+      return subGroups;
     }
 
     @Override
     public void addNote(Note n) {
-      notes.add(n);
+      ownnotes.add(n);
+      undernotes.add(n);
       return;
     }
 
     @Override
     public void addSubgroup(NoteGroup g) {
-      // TODO 自動生成されたメソッド・スタブ
+      if(g instanceof ApexDataGroup) ((ApexDataGroup)g).groupParent = this;
+      undernotes.addAll(g.getAllNotes());
+      subGroups.add(g);
+      groupcollection.add((ApexDataGroup) g);
       return;
     }
 
     @Override
     public void makeSubgroup(List<Note> notes) {
       // TODO 自動生成されたメソッド・スタブ
+      ApexDataGroup g = new ApexDataGroup();
+      g.groupParent = this;
+      g.notes.addAll(notes);
+      groupcollection.add(g);
+      
       return;
     }
 
