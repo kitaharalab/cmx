95e5c3a5710c800ab13532b059ae16f6605c1833
ntotani <ntotani@edfa4d69-1941-0410-9382-b3d123132320>
2010-01-27 13:04:01 +0000

    git-svn-id: svn+ssh://svn.osdn.net/svnroot/cmx/trunk@534 edfa4d69-1941-0410-9382-b3d123132320

--- src/jp/crestmuse/cmx/sound/VirtualKeyboard.java
+++ src/jp/crestmuse/cmx/sound/VirtualKeyboard.java
@@ -1,5 +1,10 @@
 package jp.crestmuse.cmx.sound;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
@@ -14,6 +19,10 @@ import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Transmitter;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import jp.crestmuse.cmx.amusaj.sp.MidiInputModule;
@@ -24,8 +33,43 @@ public class VirtualKeyboard extends JFrame implements MidiDevice {
 
   public static int VELOCITY = 127;
   public static int BASE_NOTE_NUM = 60;
+  private char[] keys = { 'a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h',
+      'u', 'j', 'k', 'o', 'l', 'p', ';', ':' };
+  private int[] elevent2keys = { 0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17 };
+  private int[] seven2keys = { 1, 3, 6, 8, 10, 13, 15 };
   private boolean isOpen = false;
   private Transmitter transmitter = new VirtualKeyboardTransmitter();
+  private boolean[] pressed = new boolean[256];
+
+  public VirtualKeyboard() {
+    JMenuItem up = new JMenuItem("up");
+    up.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        BASE_NOTE_NUM += 12;
+        updateTitle();
+      }
+    });
+    JMenuItem down = new JMenuItem("down");
+    down.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        BASE_NOTE_NUM -= 12;
+        updateTitle();
+      }
+    });
+    JMenu octave = new JMenu("Octave");
+    octave.add(up);
+    octave.add(down);
+    JMenuBar menuBar = new JMenuBar();
+    menuBar.add(octave);
+    setJMenuBar(menuBar);
+    updateTitle();
+    add(new KeyboardPanel());
+    pack();
+  }
+
+  private void updateTitle() {
+    setTitle("C" + (BASE_NOTE_NUM / 12 - 2));
+  }
 
   public void close() {
     isOpen = false;
@@ -74,67 +118,51 @@ public class VirtualKeyboard extends JFrame implements MidiDevice {
     isOpen = true;
   }
 
-  private class VirtualKeyboardTransmitter implements Transmitter{
+  private class VirtualKeyboardTransmitter implements Transmitter {
 
     private Receiver receiver = null;
     private HashMap<Character, Integer> key2num;
 
-    public VirtualKeyboardTransmitter(){
+    public VirtualKeyboardTransmitter() {
       receiver = null;
       key2num = new HashMap<Character, Integer>();
-      key2num.put('z', BASE_NOTE_NUM);
-      key2num.put('s', BASE_NOTE_NUM + 1);
-      key2num.put('x', BASE_NOTE_NUM + 2);
-      key2num.put('d', BASE_NOTE_NUM + 3);
-      key2num.put('c', BASE_NOTE_NUM + 4);
-      key2num.put('v', BASE_NOTE_NUM + 5);
-      key2num.put('g', BASE_NOTE_NUM + 6);
-      key2num.put('b', BASE_NOTE_NUM + 7);
-      key2num.put('h', BASE_NOTE_NUM + 8);
-      key2num.put('n', BASE_NOTE_NUM + 9);
-      key2num.put('j', BASE_NOTE_NUM + 10);
-      key2num.put('m', BASE_NOTE_NUM + 11);
-
-      key2num.put('q', BASE_NOTE_NUM + 12);
-      key2num.put('2', BASE_NOTE_NUM + 13);
-      key2num.put('w', BASE_NOTE_NUM + 14);
-      key2num.put('3', BASE_NOTE_NUM + 15);
-      key2num.put('e', BASE_NOTE_NUM + 16);
-      key2num.put('r', BASE_NOTE_NUM + 17);
-      key2num.put('5', BASE_NOTE_NUM + 18);
-      key2num.put('t', BASE_NOTE_NUM + 19);
-      key2num.put('6', BASE_NOTE_NUM + 20);
-      key2num.put('y', BASE_NOTE_NUM + 21);
-      key2num.put('7', BASE_NOTE_NUM + 22);
-      key2num.put('u', BASE_NOTE_NUM + 23);
-
-      SwingUtilities.invokeLater(new Runnable(){
+      for (int i = 0; i < keys.length; i++)
+        key2num.put(keys[i], i);
+      SwingUtilities.invokeLater(new Runnable() {
         public void run() {
-          addKeyListener(new KeyListener(){
-            private boolean[] pressed = new boolean[256];
+          addKeyListener(new KeyListener() {
             public void keyPressed(KeyEvent e) {
-              if(receiver == null || !key2num.containsKey(e.getKeyChar())) return;
-              if(pressed[e.getKeyChar()]) return;
+              if (receiver == null || !key2num.containsKey(e.getKeyChar()))
+                return;
+              if (pressed[e.getKeyChar()])
+                return;
               try {
                 ShortMessage sm = new ShortMessage();
-                sm.setMessage(ShortMessage.NOTE_ON, key2num.get(e.getKeyChar()), VELOCITY);
+                sm.setMessage(ShortMessage.NOTE_ON, key2num.get(e.getKeyChar())
+                    + BASE_NOTE_NUM, VELOCITY);
                 receiver.send(sm, -1);
               } catch (InvalidMidiDataException e1) {
                 e1.printStackTrace();
               }
               pressed[e.getKeyChar()] = true;
+              repaint();
             }
+
             public void keyReleased(KeyEvent e) {
-              if(receiver == null || !key2num.containsKey(e.getKeyChar())) return;
+              if (receiver == null || !key2num.containsKey(e.getKeyChar()))
+                return;
               try {
                 ShortMessage sm = new ShortMessage();
-                sm.setMessage(ShortMessage.NOTE_OFF, key2num.get(e.getKeyChar()), VELOCITY);
+                sm.setMessage(ShortMessage.NOTE_OFF,
+                    key2num.get(e.getKeyChar()) + BASE_NOTE_NUM, VELOCITY);
                 receiver.send(sm, -1);
               } catch (InvalidMidiDataException e1) {
                 e1.printStackTrace();
               }
               pressed[e.getKeyChar()] = false;
+              repaint();
             }
+
             public void keyTyped(KeyEvent e) {
             }
           });
@@ -156,7 +184,44 @@ public class VirtualKeyboard extends JFrame implements MidiDevice {
     }
   }
 
-  public static void main(String[] args){
+  private class KeyboardPanel extends JPanel {
+
+    KeyboardPanel() {
+      setPreferredSize(new Dimension(640, 240));
+    }
+
+    public void paint(Graphics g) {
+      super.paint(g);
+      Color green = new Color(0, 1, 0, 0.5f);
+      for (int i = 0; i < 11; i++) {
+        g.setColor(Color.BLACK);
+        int x = getWidth() * (i + 1) / 11;
+        g.drawLine(x, 0, x, getHeight());
+        if (pressed[keys[elevent2keys[i]]]) {
+          g.setColor(green);
+          g.fillRect(x - getWidth() / 11, 0, getWidth() / 11, getHeight());
+        }
+        g.setColor(Color.BLACK);
+        g.drawString(keys[elevent2keys[i]] + "", x - getWidth() / 11 + 2,
+            getHeight() - 5);
+      }
+      float[] points = { 0.5f, 1.5f, 3.5f, 4.5f, 5.5f, 7.5f, 8.5f };
+      for (int i = 0; i < points.length; i++) {
+        g.setColor(Color.BLACK);
+        int x = (int) (getWidth() * points[i] / 11);
+        g.fillRect(x, 0, getWidth() / 11 - 2, getHeight() * 2 / 3);
+        if (pressed[keys[seven2keys[i]]]) {
+          g.setColor(green);
+          g.fillRect(x, 0, getWidth() / 11 - 2, getHeight() * 2 / 3);
+        }
+        g.setColor(Color.WHITE);
+        g.drawString(keys[seven2keys[i]] + "", x + 2, getHeight() * 2 / 3 - 5);
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
     try {
       VirtualKeyboard vk = new VirtualKeyboard();
       MidiInputModule mi = new MidiInputModule(vk);
